0bf3d9ad5728fac30ab93d05f5e93b929d132551
ryck-developer <88977217+ryck-developer@users.noreply.github.com>
2023-01-02 08:37:04 -0300

    1 file modified

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Nuxt Starter',
+    title: 'Afili-Pay',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'pt-br',
     },
     meta: [
       { charset: 'utf-8' },
