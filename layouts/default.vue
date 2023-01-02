d6b7c844e6e4a80996e4bd23a21be644f2c4020b
ryck-developer <88977217+ryck-developer@users.noreply.github.com>
2023-01-01 23:40:45 -0300

    modal com formulário

--- layouts/default.vue
+++ layouts/default.vue
@@ -79,7 +79,72 @@
           ></button>
         </header>
         <section class="modal-card-body">
-          OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+          <div>
+            <div class="p-6 shadow rounded content">
+              <h2 class="section-title">Got Any Questions</h2>
+              <form method="POST" action="#">
+                <div class="columns is-multiline">
+                  <div class="form-group column is-6-desktop">
+                    <label for="fname">First Name</label>
+                    <input
+                      type="text"
+                      class="input"
+                      id="fname"
+                      name="fname"
+                      placeholder="Your First Name"
+                      required=""
+                    />
+                  </div>
+                  <div class="form-group column is-6-desktop">
+                    <label for="lname">Last Name</label>
+                    <input
+                      type="text"
+                      class="input"
+                      id="lname"
+                      name="lname"
+                      placeholder="Your Last Name"
+                    />
+                  </div>
+                  <div class="form-group column is-12">
+                    <label for="email">Email</label>
+                    <input
+                      type="email"
+                      class="input"
+                      id="email"
+                      name="email"
+                      placeholder="Your Email Address"
+                      required=""
+                    />
+                  </div>
+                  <div class="form-group column is-12">
+                    <label for="reason">Reason of Contact</label>
+                    <select
+                      id="reason"
+                      class="input custom-select"
+                      name="reason"
+                      required=""
+                    >
+                      <option selected="" disabled="">
+                        Choose Your Reason
+                      </option>
+                      <option value="business">Business</option>
+                      <option value="ticket">Ticket</option>
+                      <option value="project">Project</option>
+                    </select>
+                  </div>
+                  <div class="form-group column is-12">
+                    <label for="message">Start Writing From Here</label>
+                    <textarea
+                      name="message"
+                      id="message"
+                      class="input"
+                      placeholder="Your Text here ..."
+                    ></textarea>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </section>
         <footer class="modal-card-foot">
           <button class="button is-success">Save changes</button>
